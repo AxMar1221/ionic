2b77bcae7fe0e855412f6867f65d33a730e27a65
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-31 22:48:25 -0500

    pipe completed

---
+++ src/app/pipes/pipes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FilterPipe } from './filter.pipe';
+
+@NgModule({
+  declarations: [
+    FilterPipe
+  ],
+  exports: [
+    FilterPipe
+  ]
+})
+export class PipesModule { }
